97561c7de85768b623b7d9b346aa4fa0cd1946b1
sudha6 <sudhamadhu1100@gmail.com>
2019-12-17 19:50:52 +0530

    Python programs using list added

---
+++ list.py
@@ -0,0 +1,10 @@
+list1 = ['briyani', "icecream", 'mango', "chocolate"]
+
+print (list1)
+list2 = ["27", "72"]
+
+#print(list2)
+
+list3 = list1 + list2
+
+print(list3) 
---
+++ list_or_not.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+'''
+Created on 
+Course work: 
+@author: Sudha
+Source:
+    
+'''
+
+# Import necessary modules
+
+
+
+
+
+
+
+
+def startpy():
+    my_list = ['purnima','samyuktha', 'preethi','sudha']
+    if my_list == []:
+        print ('list is empty')
+    else:
+        print ('''list isn't empty''')
+
+
+if __name__ == '__main__':
+    startpy()
